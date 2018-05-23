7c5838467918fc22474bc71f5518050469c3a7ef
jqz <jqz@163.com>
2018-05-23 09:48:26 +0800

    joke

--- jqz-first/pages/joke/joke.js
+++ jqz-first/pages/joke/joke.js
@@ -1,82 +1,71 @@
-// pages/test2/test2.js
+// pages/joke/joke.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title: "湖南工程学院",
-    url: 'pages/test2/test2',
-    name:'波妞',
-    items: [{
-        title: '上海',
-        food: '生煎包'
-      },
-      {
-        title: '北京',
-        food: '烤鸭',
-      },
-      {
-        title: '河南',
-        food: '烩面'
-      }
-    ]
+  
   },
 
   /**
-   * 生命周期函数--监听页面加载相当于window.onload
-   * 
+   * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+     wx.request({     
+       url:'https://v.juhe.cn/joke/img/list.phpkev=2ca3e19979fadf2279ac6a036669f5ad&page=1&pagesize=2&sort=asc&time=1418745237',
+       success:function(res){
+         console.log(res);
+       }
+     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
