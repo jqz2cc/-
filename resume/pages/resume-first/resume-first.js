e5b5451481360d1f4e3bdd3a39407bf60e943060
jqz2cc@163.com <jqz2cc@163.com>
2018-05-26 18:04:00 +0800

    resume

---
+++ resume/pages/resume-first/resume-first.js
@@ -0,0 +1,102 @@
+// pages/resume-first/resume-first.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgs: [{
+        url: "/images/Plum-1.png",
+        text: "点击查看信息"
+      },
+      {
+        url: "/images/orchid.png",
+        text: "点击查看信息"
+      },
+      {
+        url: '/images/bamboo-1.png',
+        text: "点击查看信息"
+      },
+      {
+        url: '/images/chrysanthemum.png',
+        text: "点击查看信息"
+      },
+      {
+        url: '',
+        text: "点击重新排序"
+      },
+      {
+        url: "/images/Piano.png",
+        text: "点击查看信息"
+      },
+      {
+        url: '/images/chess.png',
+        text: "点击查看信息"
+      },
+      {
+        url: '/images/book.png',
+        text: "点击查看信息"
+      },
+      {
+        url: '/images/painting.png',
+        text: "点击查看信息"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
